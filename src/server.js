82e42a8fb550e0826cd04e122e9dc22f060dcfec
Ruan Carlos <ruan.carlos@dce.ufpb.br>
2019-10-01 21:28:19 -0300

    MC

--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.json({
-        message: "Hello H"
-    });
-});
+mongoose.connect('mongodb://ruaanc:12345@cluster0-shard-00-00-wshpy.mongodb.net:27017,cluster0-shard-00-01-wshpy.mongodb.net:27017,cluster0-shard-00-02-wshpy.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
