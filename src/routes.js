b7a6e2685a7994d5d3cae188760166368d76ad61
Ruan Carlos <ruan.carlos@dce.ufpb.br>
2019-10-01 23:07:31 -0300

    Dashboard

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,17 @@ const multer = require('multer');
 const uploadConfig = require('./config/upload');
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
+const Dashboard = require('./controllers/DashboardController');
 
 const routes = express .Router();
 const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store);
 
+
 routes.get('/spots', SpotController.index);
 routes.post('/spots', upload.single('thumbnail'),SpotController.store);
+
+routes.get('/dashboard', Dashboard.show);
 
 module.exports = routes;
